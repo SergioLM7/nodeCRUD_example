745ba48cf62027e95c727eab52314ba993600089
Sergio Lillo <sergiolillom@gmail.com>
2026-02-13 21:50:18 +0100

    CRUD 100% completed

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const { Pool } = require('pg');
 
 //Objeto de conexión a la base de datos
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: Number(proccess.env.DB_PORT),
-    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
 });
